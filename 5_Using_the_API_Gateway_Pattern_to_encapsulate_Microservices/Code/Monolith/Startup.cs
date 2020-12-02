a71623fba4ee220f68156a393f4cda093df8dc81
RPM1984 <rpmir1@gmail.com>
2020-12-02 16:06:10 +1100

    Updated to .NET 5

--- 5_Using_the_API_Gateway_Pattern_to_encapsulate_Microservices/Code/Monolith/Startup.cs
+++ 5_Using_the_API_Gateway_Pattern_to_encapsulate_Microservices/Code/Monolith/Startup.cs
@@ -19,7 +19,7 @@ namespace Monolith
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<Repository>(new Repository());
+            services.AddSingleton<Repository>();
             services.AddControllers();
         }
 
